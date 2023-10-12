55fb874da585a53d7c9d483b2096238537e7ba8a
ssongseulki <ssongseulki@naver.com>
2023-10-12 22:50:58 +0900

    Feat: Service - ServiceImpl 구조 수정
     Service - ServiceImpl 구조를 사용하여 인터페이스와 구현체를 분리함으로써 구현체는 독립적으로 변경하거나 확장하여도 사용하는 클라이언트에는 영향을 주지 않도록 설계 변경

--- src/main/java/ssong/boardspring/service/MemberService.java
+++ src/main/java/ssong/boardspring/service/MemberService.java
@@ -1,71 +1,26 @@
 package ssong.boardspring.service;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ssong.boardspring.domain.Member;
 import ssong.boardspring.dto.MemberCreateDto;
-import ssong.boardspring.repository.MemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Transactional
-@Service
-public class MemberService {
-
-    private MemberRepository memberRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    public MemberService(MemberRepository memberRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.memberRepository = memberRepository;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
-
+public interface MemberService {
     //회원가입
-    public boolean join(MemberCreateDto memberCreateDto) {
-        memberCreateDto.setMemberPw(bCryptPasswordEncoder.encode(memberCreateDto.getMemberPw()));
-        boolean isDuplicate = checkDuplicateMember(memberCreateDto.toEntity());
-        if (isDuplicate) {
-            return false;
-        } else {
-            memberRepository.save(memberCreateDto.toEntity());
-            return true;
-        }
-
-    }
-
-    //중복회원 검증
-    public boolean checkDuplicateMember(Member member) {
-        return memberRepository.findBymemberEmail(member.getMemberEmail()).isPresent();
-    }
+    boolean join(MemberCreateDto memberCreateDto);
 
     //전체 회원 조회
-    public List<Member> findMembers() {
-        return memberRepository.findAll();
-    }
+    List<Member> findMembers();
 
     //전체 회원 조회 For Paging
-    public Page<Member> findMembersForPaging(int page, int pageSize) {
-        Pageable pageable = PageRequest.of(page - 1, pageSize, Sort.by("id").descending());
-        return memberRepository.findAll(pageable);
-    }
+    Page<Member> findMembersForPaging(int page, int pageSize);
 
-    //ID로 단일 회원 조회
-    public Optional<Member> findOne(Long memberId) {
-        return memberRepository.findById(memberId);
-    }
+    //회원 조회 By ID
+    Optional<Member> findOne(Long memberId);
 
-    //Email로 회원 정보 조회
-    public Member findByMemberEmail(String memberEmail) {
-        Optional<Member> optionalMember = memberRepository.findBymemberEmail(memberEmail);
-        if (optionalMember.isPresent()) {
-            return optionalMember.get();
-        }
-        return null;
-    }
+    //회원 정보 조회 By Email
+    Member findByMemberEmail(String memberEmail);
+
 }
